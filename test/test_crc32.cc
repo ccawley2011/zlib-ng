42367d922cf894dd82eb0cf41884793d497c3bfa
Cameron Cawley <ccawley2011@gmail.com>
2024-02-29 18:34:01 +0000

    Rename most ACLE references to ARMv8

--- test/test_crc32.cc
+++ test/test_crc32.cc
@@ -212,8 +212,8 @@ TEST_CRC32(braid, PREFIX(crc32_braid), 1)
 TEST_CRC32(native, native_crc32, 1)
 #else
 
-#ifdef ARM_ACLE
-TEST_CRC32(acle, crc32_acle, test_cpu_features.arm.has_crc32)
+#ifdef ARM_CRC32
+TEST_CRC32(armv8, crc32_armv8, test_cpu_features.arm.has_crc32)
 #endif
 #ifdef POWER8_VSX_CRC32
 TEST_CRC32(power8, crc32_power8, test_cpu_features.power.has_arch_2_07)
